2a4049f44109b71df9d5ee5354e639179c90319b
Joseph Stahl <josephst18+git@outlook.com>
2016-12-08 19:03:27 -0600

    wasted so much time

--- data/parse.js
+++ data/parse.js
@@ -1,13 +1,5 @@
 const fs = require('fs');
-const xml2js = require('xml2js');
 
-const parser = new xml2js.Parser();
-fs.readFile(__dirname + '/full-database.xml', (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  parser.parseString(data, (err, result) => {
-    console.dir(result);
-    console.log('done');
-  });
+fs.readFile(__dirname + '/small.xml', 'utf-8', (err, data) => {
+  console.log(data);
 });
