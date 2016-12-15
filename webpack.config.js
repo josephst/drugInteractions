93b5ee2d7e882328775971b902944d1cc95b9364
Joseph Stahl <josephst18+git@outlook.com>
2016-12-15 00:30:39 -0600

    latest webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,25 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: './src/index.tsx',
     output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
     },
 
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: 'source-map',
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
     },
 
     module: {
-        loaders: [
+        rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
+            { test: /\.js$/, enforce: 'pre', loader: 'source-map-loader' },
+        ],
     },
 
     // When importing a module whose path matches one of the following, just
@@ -30,7 +27,7 @@ module.exports = {
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+        'react': 'React',
+        'react-dom': 'ReactDOM',
     },
 };
